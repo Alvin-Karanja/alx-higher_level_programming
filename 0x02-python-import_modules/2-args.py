aef7ce636a197f4d66e0af412babcd593f129922
Alvin-Karanja <alvin2@protonmail.com>
2022-06-02 14:03:09 +0300

    My commit on 0x02-python import_modules project task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-
-argc = len(argv)
-
-if __name__ == '__main__':
-    if argc == 1:
-        print(f"{argc - 1} arguments.")
-    elif argc == 2:
-        print(f"{argc - 1} argument:")
-    else:
-        print(f"{argc - 1} arguments:")
-
-    index = 1
-    while index < argc:
-        print(f"{index}: {argv[index]}")
-        index += 1
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv)
+    print("{} ".format(args - 1), end="")
+    if args == 1:
+        print("arguments.")
+    elif args > 1:
+        if args == 2:
+            print("argument:")
+        elif args > 2:
+            print("arguments:")
+        for i in range(args - 1):
+            print("{}: {}".format((i + 1), sys.argv[i + 1]))
